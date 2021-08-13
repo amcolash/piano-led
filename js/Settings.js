083dc5e09c394a725f1a9caf105221785f494dd4
amcolash <amcolash@gmail.com>
2021-08-13 02:02:39 +0000

    Deploying to gh-pages from @ amcolash/piano-led@343db3abf52b13aace0f9cca43ab08130dfdded8 🚀

--- js/Settings.js
+++ js/Settings.js
@@ -1,23 +1,21 @@
 import { html } from 'https://unpkg.com/htm/preact/standalone.module.js';
 
 import { sliders } from './icons.js';
-import { Server, title } from './util.js';
+import { Server } from './util.js';
 
 export default function Settings(props) {
-  const folders = props.status.folders;
-  const musicRoot = props.status.musicRoot;
+  const palettes = props.status.palettes;
 
   return html`
     <div class="icon" style=${{ position: 'absolute', top: '1rem', right: '1rem' }}>
       ${sliders}
       <select
         class="icon"
-        style=${{ position: 'absolute', top: 0, left: 0 }}
-        onChange=${(e) => fetch(`${Server}/play?folder=${e.target.value}`).then(() => setTimeout(props.getData, 500))}
+        style=${{ position: 'absolute', top: 0, left: 0, color: 'rgba(0,0,0,0)' }}
+        onChange=${(e) => fetch(`${Server}/palette?value=${e.target.value}`).then(() => setTimeout(props.getData, 500))}
         value=${null}
       >
-        <option value=${musicRoot}>All Music</option>
-        ${folders.map((f) => html`<option value=${f}>${title(f.replace(musicRoot + '/', ''))}</option>`)}
+        ${palettes.map((p) => html`<option value=${p}>${p}</option>`)}
       </select>
     </div>
   `;
