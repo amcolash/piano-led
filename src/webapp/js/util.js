846c80fa33c82a166a788346b4d43ce138153522
Andrew McOlash <amcolash@gmail.com>
2021-08-12 18:40:38 -0700

    start work on settings, make icons consistent

--- src/webapp/js/util.js
+++ src/webapp/js/util.js
@@ -1,6 +1,6 @@
 export const Palette = ['#1981a4', '#43adbb', '#8bd0cf', '#f7e5d2', '#dec8b8'];
 
-export const Server = 'http://192.168.1.116:8080';
+export const Server = 'https://home.amcolash.com:9090/piano-led';
 
 // function from https://stackoverflow.com/a/22193094/2303432
 export function title(val) {
