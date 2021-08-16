c49094efb7960cb7f79b899d998d8df20e72d716
Andrew McOlash <amcolash@gmail.com>
2021-08-16 13:25:02 -0700

    visual tweaks, make the buttons more accessible w/ keyboard nav + focus

--- src/webapp/js/Folder.js
+++ src/webapp/js/Folder.js
@@ -1,6 +1,7 @@
 import { html, useState } from 'https://unpkg.com/htm/preact/standalone.module.js';
 import { shuffle, volume2, x } from './icons.js';
 
+import { Button } from './Button.js';
 import { Palette, Server, title } from './util.js';
 
 export default function Folder(props) {
@@ -21,7 +22,7 @@ export default function Folder(props) {
         background: Palette[2],
         boxShadow: '0 0 2rem rgba(0,0,0,0.3)',
         overflow: 'hidden',
-        borderRadius: '0.25rem',
+        borderRadius: '0.5rem',
       }}
     >
       <div style=${{ color: Palette[0] }}>Select Music</div>
@@ -36,22 +37,26 @@ export default function Folder(props) {
         <div class="select" style=${{ width: '30%', marginRight: '0.5rem' }}>
           ${folders.map(
             (f) =>
-              html`<div class=${`option ${selectedFolder === f ? 'selected' : ''}`} onClick=${(e) => setSelectedFolder(f)}>
+              html`<${Button} class=${`option ${selectedFolder === f ? 'selected' : ''}`} onClick=${(e) => setSelectedFolder(f)}>
                 ${title(f.replace(musicRoot + '/', '').replace(musicRoot, 'All Music'))}
-              </div>`
+              </${Button}>`
           )}
         </div>
         <div class="select" style=${{ width: '100%' }}>
-          <div class="option" onClick=${(e) => fetch(`${Server}/play?folder=${selectedFolder}`).then(() => setTimeout(props.getData, 500))}>
+          <${Button}
+            class="option"
+            onClick=${(e) => fetch(`${Server}/play?folder=${selectedFolder}`).then(() => setTimeout(props.getData, 500))}
+          >
             <div style=${{ width: '1.5rem', height: '1.5rem', marginRight: '1rem' }}>${shuffle}</div>
             <div>Shuffle Folder</div>
-          </div>
+          </${Button}>
           ${props.musicData.files[selectedFolder].map((f) => {
             const isPlaying = props.status.music && f.toLowerCase().indexOf(props.status.music.toLowerCase()) !== -1;
-            return html`<div class="option" onClick=${(e) => fetch(`${Server}/play?file=${f}`).then(() => setTimeout(props.getData, 500))}>
+            return html`<${Button} class="option" onClick=${(e) =>
+              fetch(`${Server}/play?file=${f}`).then(() => setTimeout(props.getData, 500))}>
               ${isPlaying && html`<div style=${{ width: '1.5rem', height: '1.5rem', marginRight: '1rem' }}>${volume2}</div>`}
-              <div style=${{ marginLeft: !isPlaying ? '2.5rem' : undefined }}>${title(f.replace(selectedFolder + '/', ''))}</div>
-            </div>`;
+              <div style=${{ marginLeft: !isPlaying ? '2.5rem' : undefined }}>${f.replace(selectedFolder + '/', '')}</div>
+            </${Button}>`;
           })}
         </div>
       </div>
